0bbb577b4b97c1a26f1004d2677ceb2c374c85d2
Brian Barth <bsbarth101@yahoo.com>
2019-01-08 14:42:09 -0600

    making first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CartItemsComponent } from './cart-items/cart-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartItemsComponent
   ],
   imports: [
     BrowserModule
